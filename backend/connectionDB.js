8b9f4648f71944574b8a73e2dd7a171b3b3d2fb2
Huu Tho Nguyen <huutho1999@gmail.com>
2022-05-16 03:12:43 +0700

    Create ConnectionString and connect to MongoDB

--- backend/connectionDB.js
+++ backend/connectionDB.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
-const connectDB = () => {};
+const URI =
+  "mongodb+srv://thnguyen0707:Huutho!0707@clusteronlinephoneselli.ammbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+const connectDB = () => {
+  mongoose
+    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to Database.");
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+};
+
+//TODO: Handle close connect to DB timeout with 300seconds
 const closeDB = () => {};
 
 export { connectDB, closeDB };
